7368519b447c4402a1ed4f936bf2b6d0d0d486bd
matheus-lincon <blatatlygod09@gmail.com>
2022-07-01 17:29:22 -0300

    update js code

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,18 @@
+/* GET JSON DATA */
+
+async function getDashBoardData() {
+  return await fetch('./data.json').then((res) => res.json())
+}
+let dashboardData = getDashBoardData()
+
 /* EVENT LISTENERS */
 
 let reports = document.querySelectorAll('.report')
 let reportStatus = 'weekly' // standart status
+
+window.onload = () => {
+  updateDashBoard(reportStatus)
+}
 
 reports.forEach((report) => {
   report.addEventListener('click', () => {
@@ -14,34 +25,26 @@ reports.forEach((report) => {
     report.classList.toggle('active')
     reportStatus = report.innerText.toLowerCase()
 
-    fillDashBoard(reportStatus)
+    updateDashBoard(reportStatus)
   })
 })
 
-/* --- */
+function updateDashBoard(status) {
+  let cardHours = document.querySelectorAll('.card .hours')
+  let cardLastWeek = document.querySelectorAll('.card .last-week-hours')
 
-function fillDashBoard(status) {
-  fetch('./data.json')
-    .then((response) => {
-      return response.json()
+  dashboardData.then((data) => {
+    data.forEach((e, index) => {
+      if (status === 'daily') {
+        cardHours[index].innerText = `${e.timeframes.daily.current}hrs`
+        cardLastWeek[index].innerText = `${e.timeframes.daily.current}hrs`
+      } else if (status === 'weekly') {
+        cardHours[index].innerText = `${e.timeframes.weekly.current}hrs`
+        cardLastWeek[index].innerText = `${e.timeframes.weekly.current}hrs`
+      } else {
+        cardHours[index].innerText = `${e.timeframes.monthly.current}hrs`
+        cardLastWeek[index].innerText = `${e.timeframes.monthly.current}hrs`
+      }
     })
-    .then((jasondata) => {
-      let cardHours = document.querySelectorAll('.card .hours')
-      let cardLastWeek = document.querySelectorAll('.card .last-week-hours')
-
-      jasondata.forEach((e, index) => {
-        if (status == 'daily') {
-          cardHours[index].innerText = `${e.timeframes.daily.current}hrs`
-          cardLastWeek[index].innerText = `${e.timeframes.daily.previous}hrs`
-        } else if (status == 'weekly') {
-          cardHours[index].innerText = `${e.timeframes.weekly.current}hrs`
-          cardLastWeek[index].innerText = `${e.timeframes.weekly.previous}hrs`
-        } else {
-          cardHours[index].innerText = `${e.timeframes.monthly.current}hrs`
-          cardLastWeek[index].innerText = `${e.timeframes.monthly.previous}hrs`
-        }
-      })
-    })
+  })
 }
-
-fillDashBoard(reportStatus)
